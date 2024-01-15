7cade3885f936339e3a2a151c5ffd9335fe16cc6
ffa <falsh.tester@gmail.com>
2024-01-15 10:37:15 +0330

    Initial Commit

---
+++ firstTask/firstTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstTaskApp.swift
+//  firstTask
+//
+//  Created by Farzin macbook on 1/15/24.
+//
+
+import SwiftUI
+
+@main
+struct firstTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
